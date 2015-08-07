afd5410a2f62bfbc32e72ec0c88bdedc12f1ad87
Georgiana <georgiana.paiu@vuzum.com>
2015-08-07 16:04:24 +0300

    change window plugin name

--- js/widgetic_tinymce_plugin.js
+++ js/widgetic_tinymce_plugin.js
@@ -24,7 +24,7 @@
 
 		function widgeticBox(opts){
 			popup = editor.windowManager.open( {
-				title: 'Widgetic plugin',
+				title: 'Widgetic for WordPress',
 				width: window.innerWidth*.9,
 				height: Math.min(740, window.innerHeight*.9),
 				resizable: true,
